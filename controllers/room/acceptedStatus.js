37e1834bd775e2078097fd697aa5ec3861fcdf68
aguspray001 <aguspray001@gmail.com>
2024-02-07 12:32:30 +0700

    feat: add acc status controllers and routes

--- controllers/room/acceptedStatus.js
+++ controllers/room/acceptedStatus.js
@@ -1,32 +1,106 @@
+const { Op } = require("sequelize");
+
+const AcceptedStatus = require("../../models").AcceptedStatus;
+
 module.exports = {
-    create: async (req, res) => {
-      const { accepted_status_name, accepted_status_number } = req.body;
-      // check room_name in database
-      //add to database
-    },
-  
-    read: async (req, res) => {
-      const { accepted_status_id } = req.body;
-      // check user in database
-      // if user exist
-      // password validation
-      // then return token
-    },
-  
-    update: async (req, res) => {
-      const { accepted_status_id } = req.body;
-      // check user in database
-      // if user exist
-      // password validation
-      // then return token
-    },
-  
-    delete: async (req, res) => {
-      const { accepted_status_id } = req.body;
-      // check user in database
-      // if user exist
-      // password validation
-      // then return token
-    },
-  };
-  
+  create: async (req, res) => {
+    try {
+      const { status_name, status_number } = req.body;
+      const data = { status_name, status_number };
+
+      // check status_name in database
+      const room = await AcceptedStatus.findOne({
+        where: {
+          status_name: { [Op.eq]: status_name },
+        },
+      });
+      if (!room) {
+        //add to database
+        const resp = await AcceptedStatus.create(data);
+        res.status(200).send({
+          data: resp,
+          message: "Success to add data to database",
+          status: 200,
+        });
+      } else {
+        throw new Error("AcceptedStatus has been registered to database");
+      }
+    } catch (e) {
+      res.status(400).send({ data: null, message: e.message, status: 400 });
+    }
+  },
+
+  read: async (req, res) => {
+    try {
+      const rooms = await AcceptedStatus.findAll();
+      if (rooms) {
+        res.status(200).send({
+          data: rooms,
+          message: "Success to read data from database",
+          status: 200,
+        });
+      }else{
+        throw new Error("Failed to read data from database");
+      }
+    } catch (e) {
+      res.status(200).send({
+        data: null,
+        message: e.message,
+        status: 200,
+      });
+    }
+  },
+
+  update: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const { status_name, status_number } = req.body;
+      const data = { status_name, status_number };
+
+      const room = await AcceptedStatus.findByPk(id);
+
+      if (!room) {
+        throw new Error("AcceptedStatus was not found");
+      } else {
+        const resp = await AcceptedStatus.update(data, { where: { id } });
+        res.status(200).send({
+          data: resp,
+          message: "AcceptedStatus has been successfuly updated to database",
+          status: 200,
+        });
+      }
+    } catch (e) {
+      res.status(500).send({
+        data: null,
+        message: e.message,
+        status: 500,
+      });
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const room = await AcceptedStatus.findByPk(id);
+
+      if (!room) {
+        throw new Error("AcceptedStatus was not found");
+      } else {
+        const resp = await AcceptedStatus.destroy({ where: { id } });
+        res.status(200).send({
+          data: resp,
+          message: "AcceptedStatus has been successfuly deleted to database",
+          status: 200,
+        });
+      }
+    } catch (e) {
+      res.status(500).send({
+        data: null,
+        message: e.message,
+        status: 500,
+      });
+    }
+  },
+};
--- routers/room.js
+++ routers/room.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const roomController = require("../controllers/room/room");
 const roomRentController = require("../controllers/room/roomRent");
+const accController = require("../controllers/room/acceptedStatus");
+
 
 // rent router
 router.post("/rent", roomRentController.rentRoom);
@@ -13,5 +15,11 @@ router.post("/", roomController.create);
 router.get("/", roomController.read);
 router.put("/:id", roomController.update);
 router.delete("/:id", roomController.delete);
+
+// accepted-status router
+router.post("acc/", accController.create);
+router.get("acc/", accController.read);
+router.put("acc/:id", accController.update);
+router.delete("acc/:id", accController.delete);
 
 module.exports = router;
