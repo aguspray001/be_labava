9d0f4a5b9225633077de2f218ccda47230c46763
aguspray001 <aguspray001@gmail.com>
2024-02-07 12:20:48 +0700

    feat: room controllers & routes

--- controllers/room/room.js
+++ controllers/room/room.js
@@ -1,32 +1,109 @@
+const { Op } = require("sequelize");
+
+const Room = require("../../models").Room;
+
 module.exports = {
-    create: async (req, res) => {
+  create: async (req, res) => {
+    try {
       const { room_name, room_number } = req.body;
+      const data = { room_name, room_number };
+
       // check room_name in database
-      //add to database
-    },
-  
-    read: async (req, res) => {
-      const { room_id } = req.body;
-      // check user in database
-      // if user exist
-      // password validation
-      // then return token
-    },
-  
-    update: async (req, res) => {
-      const { room_id } = req.body;
-      // check user in database
-      // if user exist
-      // password validation
-      // then return token
-    },
-  
-    delete: async (req, res) => {
-      const { room_id } = req.body;
-      // check user in database
-      // if user exist
-      // password validation
-      // then return token
-    },
-  };
-  
+      const room = await Room.findOne({
+        where: {
+          room_name: { [Op.eq]: room_name },
+        },
+      });
+      if (!room) {
+        //add to database
+        const resp = await Room.create(data);
+        res.status(200).send({
+          data: resp,
+          message: "Success to add data to database",
+          status: 200,
+        });
+      } else {
+        throw new Error("Room has been registered to database");
+      }
+    } catch (e) {
+      res.status(400).send({ data: null, message: e.message, status: 400 });
+    }
+  },
+
+  read: async (req, res) => {
+    try {
+      const rooms = await Room.findAll();
+      if (rooms) {
+        res.status(200).send({
+          data: rooms,
+          message: "Success to read data from database",
+          status: 200,
+        });
+      }else{
+        throw new Error("Failed to read data from database");
+      }
+    } catch (e) {
+      res.status(200).send({
+        data: null,
+        message: e.message,
+        status: 200,
+      });
+    }
+  },
+
+  update: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const { room_name, room_number } = req.body;
+      const data = { room_name, room_number };
+
+      const room = await Room.findByPk(id);
+
+      if (!room) {
+        throw new Error("Room was not found");
+      } else {
+        const resp = await Room.update(data, { where: { id } });
+        res.status(200).send({
+          data: resp,
+          message: "Room has been successfuly updated to database",
+          status: 200,
+        });
+      }
+    } catch (e) {
+      res.status(500).send({
+        data: null,
+        message: e.message,
+        status: 500,
+      });
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const { room_name, room_number } = req.body;
+      const data = { room_name, room_number };
+
+      const room = await Room.findByPk(id);
+
+      if (!room) {
+        throw new Error("Room was not found");
+      } else {
+        const resp = await Room.destroy({ where: { id } });
+        res.status(200).send({
+          data: resp,
+          message: "Room has been successfuly deleted to database",
+          status: 200,
+        });
+      }
+    } catch (e) {
+      res.status(500).send({
+        data: null,
+        message: e.message,
+        status: 500,
+      });
+    }
+  },
+};
--- routers/room.js
+++ routers/room.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
-const userController = require("../controllers/user/user");
-const roleController = require("../controllers/user/role");
+const roomController = require("../controllers/room/room");
 const roomRentController = require("../controllers/room/roomRent");
 
 // rent router
@@ -8,5 +7,11 @@ router.post("/rent", roomRentController.rentRoom);
 router.get("/rent/:id", roomRentController.accRentRoom);
 router.put("/rent/:id", roomRentController.rejectRentRoom);
 router.delete("/rent/:id", roomRentController.rollBackRentRoom);
+
+// room router
+router.post("/", roomController.create);
+router.get("/", roomController.read);
+router.put("/:id", roomController.update);
+router.delete("/:id", roomController.delete);
 
 module.exports = router;
